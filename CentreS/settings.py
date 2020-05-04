9d9ca63bedd456a1149bde1bd57f26932ca6de5f
Marieaimee40 <konemarieaimee@gmail.com>
2020-05-04 19:43:25 +0200

    commit

--- CentreS/settings.py
+++ CentreS/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'reversion',
     'import_export',
-    'CentreSante.apps.CentresanteConfig',
+    #'CentreSante.apps.CentresanteConfig',
 ]
 
 MIDDLEWARE = [
