b68d38926a0a890855839f69bbd9a2c7ddf01e92
Cheick-T <abdallahatyoro@gmail.com>
2020-05-15 04:25:33 +0000

    ajout de djangorestframework et restswagger

--- CentreS/settings.py
+++ CentreS/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'reversion',
     'import_export',
     'CentreApp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -101,6 +102,14 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 
 # Internationalization
